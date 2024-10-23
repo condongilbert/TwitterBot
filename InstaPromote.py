5a57e2d57168901675bbbd041f66e5c503bdc54c
Gilbert <condongilbert@gmail.com>
2024-10-23 12:16:35 -0700

    instagram giving secruity issues when trying to login

--- InstaPromote.py
+++ InstaPromote.py
@@ -17,9 +17,9 @@ bot.login(username=username, password=password)
 # Like posts with specific hashtags
 hashtags = ['peptides', 'fitness', 'bodybuilding']
 for hashtag in hashtags:
-    bot.like_hashtag(hashtag, amount=10)  # Likes 10 posts for each hashtag
+    bot.like_hashtag(hashtag, amount=1)  # Likes 1 posts for each hashtag
 
 # Follow users who posted with a specific hashtag
 for hashtag in hashtags:
     users = bot.get_hashtag_users(hashtag)
-    bot.follow_users(users[:10])  # Follow 10 users for each hashtag
+    bot.follow_users(users[:1])  # Follow 1 users for each hashtag
